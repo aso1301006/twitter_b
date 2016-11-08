e1904f454aa283918ce9db4a8f04e2b0da5112db
万理奈 <@marina>
2016-11-08 12:41:11 +0900

    階層修正

--- twi_analysis/header.php
+++ twi_analysis/header.php
@@ -15,7 +15,7 @@ session_start();
 <html lang="ja" xmlns="http://www.w3.org/1999/xhtml" xml:lang="ja">
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
-<link rel="stylesheet" type="text/css" href="css/css.css"></link>
+<link rel="stylesheet" type="text/css" href="../css/css.css"></link>
 </head>
 <body>
 	<div class="main">
