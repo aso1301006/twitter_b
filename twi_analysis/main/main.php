02c76c4e80dcf081e7b297a2639c83f21ef0ea27
student <student@PC63218.asost.local>
2016-11-08 15:27:19 +0900

    メインページ追加

---
+++ twi_analysis/main/main.php
@@ -0,0 +1,23 @@
+
+<html lang="ja" xmlns="http://www.w3.org/1999/xhtml" xml:lang="ja">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+<link rel="stylesheet" type="text/css" href="css.css"></link>
+</head>
+<body>
+<?php
+include ('../header.php');
+?>
+<div class="main">
+
+	<div id="search">
+		<img src="../img/search_you.png" alt="分析" width="300px" height="72px"></img>
+	</div>
+
+	<div id="relaod">
+		<img src="../img/reload.png" alt="再読み込み" width="50px" height="50px"></img>
+	</div>
+
+</div>
+</body>
+</html>
