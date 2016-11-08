06f888c9325d4f9c5915f2c4e7cba5341ba79488
万理奈 <@marina>
2016-11-08 14:46:15 +0900

    twitterのアカウント情報適応

--- twi_analysis/header.php
+++ twi_analysis/header.php
@@ -1,21 +1,22 @@
 <?php
-
 session_start();
-//require '../DB.php';
-//$user_name=$_SESSION['user_name'];
+
+$t_id=$_SESSION['screen_name'];
+$t_name=$_SESSION['name'];
+$t_icon=$_SESSION['profile_image_url_https'];
 
 //ログインしていないorセッションが切れた場合------------
-/*	if($user_name==null){
+	if($t_id==null){
 		header("Location: login/login.php");
 	}
-*/
-
 ?>
+
+
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html lang="ja" xmlns="http://www.w3.org/1999/xhtml" xml:lang="ja">
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
-<link rel="stylesheet" type="text/css" href="../css/css.css"></link>
+<link rel="stylesheet" type="text/css" href="css/css.css"></link>
 </head>
 <body>
 	<div class="main">
@@ -29,11 +30,11 @@ session_start();
 
 		<div id="user_info">
 			<div class="icon">
-				<a href=""><img src="img/motoi.png" alt="user_icon" width="50px" height="50px"></img></a>
+			<img src="<?php echo $t_icon;?>" alt="user_icon" width="50px" height="50px"></img>
 			</div>
 
 			<div class="info">
-				<a>@motoidayo</a><br/><a font="1em">本井の涼の助</a>
+				<a>@<?php echo $t_id?></a><br/><a><font size="5em"><?php echo $t_name;?></font></a>
 			</div>
 		</div>
 
