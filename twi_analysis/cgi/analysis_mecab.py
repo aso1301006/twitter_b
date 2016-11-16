17a09b115ef0a31abc6aafbe6f4fbcf46f2beb61
佐藤宗大 <1301006@st.asojuku.ac.jp>
2016-11-16 17:17:58 +0900

    phpからpythonを呼び出すテストファイルの作成

--- twi_analysis/cgi/analysis_mecab.py
+++ twi_analysis/cgi/analysis_mecab.py
@@ -30,40 +30,43 @@ def mecab_analysis(sentence):
     return result_dict
 
 
-# mecabedがtrueのものはすでに分解されているので除外する
-for d in tweetdata.find({'mecabed': {'$ne': True}}, {
-    '_id': 1, 'id': 1, 'text': 1, 'noun': 1,
-    'verb': 1, 'adjective': 1, 'adverb': 1
-}):
-    twitter_text = unicodedata.normalize('NFKC', d['text'])  # 半角カナを全角カナに
-    res = mecab_analysis(twitter_text)
+if __name__ == '__main__':
+    # mecabedがtrueのものはすでに分解されているので除外する
+    for d in tweetdata.find({'mecabed': {'$ne': True}}, {
+        '_id': 1, 'id': 1, 'text': 1, 'noun': 1,
+        'verb': 1, 'adjective': 1, 'adverb': 1
+    }):
+        twitter_text = unicodedata.normalize('NFKC', d['text'])  # 半角カナを全角カナに
+        res = mecab_analysis(twitter_text)
 
-    # 品詞毎にフィールド分けして入れ込んでいく
-    for k in res.keys():
-        if k == u'形容詞':  # adjective
-            adjective_list = []
-            for w in res[k]:
-                adjective_list.append(w)
-            tweetdata.update({'_id': d['_id']}, {
-                             '$push': {'adjective': {
-                                 '$each': adjective_list}}})
-        elif k == u'動詞':  # verb
-            verb_list = []
-            for w in res[k]:
-                verb_list.append(w)
-            tweetdata.update({'_id': d['_id']}, {
-                             '$push': {'verb': {'$each': verb_list}}})
-        elif k == u'名詞':  # noun
-            noun_list = []
-            for w in res[k]:
-                noun_list.append(w)
-            tweetdata.update({'_id': d['_id']}, {
-                             '$push': {'noun': {'$each': noun_list}}})
-        elif k == u'副詞':  # adverb
-            adverb_list = []
-            for w in res[k]:
-                adverb_list.append(w)
-            tweetdata.update({'_id': d['_id']}, {
-                             '$push': {'adverb': {'$each': adverb_list}}})
-    # 形態素解析済みのツイートにMecabedフラグの追加
-    tweetdata.update({'_id': d['_id']}, {'$set': {'mecabed': True}})
+        # 品詞毎にフィールド分けして入れ込んでいく
+        for k in res.keys():
+            if k == u'形容詞':  # adjective
+                adjective_list = []
+                for w in res[k]:
+                    adjective_list.append(w)
+                tweetdata.update({'_id': d['_id']}, {
+                                 '$push': {'adjective': {
+                                     '$each': adjective_list}}})
+            elif k == u'動詞':  # verb
+                verb_list = []
+                for w in res[k]:
+                    verb_list.append(w)
+                tweetdata.update({'_id': d['_id']}, {
+                                 '$push': {'verb': {'$each': verb_list}}})
+            elif k == u'名詞':  # noun
+                noun_list = []
+                for w in res[k]:
+                    noun_list.append(w)
+                tweetdata.update({'_id': d['_id']}, {
+                                 '$push': {'noun': {'$each': noun_list}}})
+            elif k == u'副詞':  # adverb
+                adverb_list = []
+                for w in res[k]:
+                    adverb_list.append(w)
+                tweetdata.update({'_id': d['_id']}, {
+                                 '$push': {'adverb': {'$each': adverb_list}}})
+        # 形態素解析済みのツイートにMecabedフラグの追加
+        tweetdata.update({'_id': d['_id']}, {'$set': {'mecabed': True}})
+
+    print 'success'
