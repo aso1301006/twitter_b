6d7f760acc968e32b99206f0a1fcde6570e16954
Hikaru <Hikaru@Hikaru-PC>
2016-11-17 08:32:09 +0900

    メイン画面とあなた画面の間の処理部分作成途中

--- twi_analysis/DBManager.php
+++ twi_analysis/DBManager.php
@@ -5,28 +5,39 @@ $mongo = new MongoClient("35.162.58.174:27017");
 // データベースの選択
 $db = $mongo->selectDB("twi_analysis");
 
-function tweets_all_search(){//ツイートデータ全件取得
+function tweets_search($array = null){//ツイートデータ取得
 	//グローバル変数宣言
 	global $mongo, $db;
-
-	// MongoDBクライアントの作成
-	$mongo = new MongoClient("35.162.58.174:27017");
-
-	// データベースの選択
-	$db = $mongo->selectDB("twi_analysis");
-
 	// コレクションの選択
 	$collection = $db->selectCollection("tweetdata");
-	//データ取得
-	$acquisition = $collection->find();
+
+	if(empty($array)){//データ取得
+		$acquisition = $collection->find();
+	}else{
+		$acquisition = $collection->find($array);
+	}
 	//返り値
 	return $acquisition;
 }
 
-function user_search($array){//ユーザー情報取得
+function tweets_count($array = null){
 	//グローバル変数宣言
 	global $mongo, $db;
+	// コレクションの選択
+	$collection = $db->selectCollection("tweetdata");
 
+	if(empty($array)){//データ取得
+		$acquisition = $collection->find()->count();
+	}else{
+		$acquisition = $collection->find($array)->count();
+	}
+	//返り値
+	return $acquisition;
+}
+
+function user_search($array = null){//ユーザー情報取得
+	//グローバル変数宣言
+	global $mongo, $db;
 	// コレクションの選択
 	$collection = $db->selectCollection("user_data");
 
@@ -40,7 +51,6 @@ function user_search($array){//ユーザー情報取得
 function tweets_one_insert($array){//ツイートDBに1件挿入
 	//グローバル変数宣言
 	global $mongo, $db;
-
 	// コレクションの選択
 	$collection = $db->selectCollection("tweetdata");
 
