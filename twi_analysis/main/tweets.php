32c6f9dd2ddb6f9a577c84988805d7db4575dbc2
student <student@PC63207.asost.local>
2016-11-17 13:25:23 +0900

    途中

---
+++ twi_analysis/main/tweets.php
@@ -0,0 +1,57 @@
+<?php
+include '../Authentication.php';
+include '../DBManager.php';
+
+//処理制限時間を無期限に
+set_time_limit(0);
+
+//初回の人かをtweetsadataを参照して判断する
+//⇒ツイートデータ内のスクリーンネームを検索してヒットしなければ初回
+//ユーザid
+$user_id = $_SESSION['id'];
+//$first = tweets_count(array("user_id"=>$user_id));
+
+// $user_id = '791505177299726336';
+$first = 0;
+
+// エンドポイント(ユーザーのタイムラインを取得する)
+$request_url = 'https://api.twitter.com/1.1/statuses/user_timeline.json' ;
+// 	パラメータA (オプション)ユーザタイムライン用
+$params_a = array();
+// 	ユーザーid
+$params_a['user_id'] = $user_id;
+// 	取得件数 1から200まで
+$params_a['count'] = '200';
+// 	ユーザー情報を除外するのか
+$params_a['trim_user'] = 'false';
+
+if($first == 0){//初回の人 800ツイート取得
+	$count = 0;//ツイート保存回数
+	for($i=0;$i<503;$i++){
+		//古いツイートidを取得している場合
+		if(isset($max_id)){$params_a['max_id'] = $max_id;}
+		//ツイート取得
+		$tweets =  Authentication($request_url, $params_a);
+		//かぶっている値を取り除く
+		if(isset($max_id)){array_shift($tweets);}
+		foreach( $tweets as $key => $value ){
+			$id = $tweets[$key]['id_str'];//ツイートid
+			$text = $tweets[$key]['text'];//ツイート内容
+			$date = date('Y年m月d日H時i分s秒',  strtotime($tweets[$key]['created_at']));//ツイート日時
+			$max_id = $id;
+			//tweetsdataにインサート
+			tweets_one_insert(array('twi_id'=>$id,'text'=>$text,'create_at'=>$date,'user_id'=>$user_id));
+			$count++;
+			if($count>=10000){break 2;}
+		}
+	}
+}else{//二回目以降の人 10万ツイート取得(200ツイート*503リクエスト)
+	//DBからsince_id取得
+	$since_id = tweets_search();
+	var_dump($since_id);
+	//DBの最新から10万ツイート取得
+
+}
+// echo($count);
+header( "Location: http://localhost/twi_analysis/your_page/your_page.php" ) ;
+?>
--- twi_analysis/main/user_get.php
+++ twi_analysis/main/user_get.php
@@ -1,62 +1,19 @@
-<?php
-include '../Authentication.php';
-include '../DBManager.php';
-
-//処理制限時間を無期限に
-set_time_limit(0);
-
-//初回の人かをtweetsadataを参照して判断する
-//⇒ツイートデータ内のスクリーンネームを検索してヒットしなければ初回
-//ユーザid
-// $user_id = $_SESSION['id'];
-$user_id = '791505177299726336';
-$first = tweets_count(array("user_id"=>$user_id));
-
-// エンドポイント(ユーザーのタイムラインを取得する)
-$request_url = 'https://api.twitter.com/1.1/statuses/user_timeline.json' ;
-// 	パラメータA (オプション)ユーザタイムライン用
-$params_a = array();
-// 	ユーザーid
-$params_a['user_id'] = $user_id;
-// 	取得件数 1から200まで
-$params_a['count'] = '200';
-// 	ユーザー情報を除外するのか
-$params_a['trim_user'] = 'false';
-
-if($first == 0){//初回の人 800ツイート取得
-	//ツイート保存回数
-	$count = 0;
-	for($i=0;$i<5;$i++){
-		//ツイート取得
-		$tweets =  Authentication($request_url, $params_a);
-		foreach( $tweets as $key => $value ){
-			$id = $tweets[$key]['id_str'];//ツイートid
-			$text = $tweets[$key]['text'];//ツイート内容
-			$date = date('Y年m月d日',  strtotime($tweets[$key]['created_at']));//ツイート日時
-			//tweetsdataにインサート
-// 			tweets_one_insert(array('twi_id'=>$id,'text'=>$text,'create_at'=>$date,'user_id'=>$user_id));
-			$count++;
-			if($count>=800){break 2;}
-		}
-	}
-}else{//二回目以降の人 10万ツイート取得(200ツイート*503リクエスト)
-	//DBからsince_id取得
-
-	//DBの最新から10万ツイート取得
-
-}
-echo $count;
-?>
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html lang="ja" xmlns="http://www.w3.org/1999/xhtml" xml:lang="ja">
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=UTF-8"></meta>
+<!--jquery.min.js読み込みは必須-->
+<script type='text/javascript' src='https://ajax.googleapis.com/ajax/libs/jquery/1.7.2/jquery.min.js'></script>
+<script type="text/javascript">
+	$(function(){
+		$("#load").load("./tweets.php");
+	})
+</script>
 </head>
 <body>
-<?php
-// 	$search = tweets_count();
-// 	var_dump(iterator_to_array($search));
-// 	echo $search;
-	?>
+<div id="load" align="center">
+	<img src="../img/ajax-loader.gif"alt="Now Loading..." />	<!--ローディング画像-->
+	<h2>ツイート情報を取得しています・・・・</h2>
+</div>
 </body>
 </html>
