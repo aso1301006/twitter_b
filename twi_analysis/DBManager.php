55385df88cc306af0d6b2a9bcf9310f61d092454
student <student@PC63207.asost.local>
2016-11-22 14:41:54 +0900

    DB更新

--- twi_analysis/DBManager.php
+++ twi_analysis/DBManager.php
@@ -5,56 +5,89 @@ $mongo = new MongoClient("35.162.58.174:27017");
 // データベースの選択
 $db = $mongo->selectDB("twi_analysis");
 
-function tweets_search($array = null){//ツイートデータ取得
+//全ての変数は配列が引数
+
+function tweets_search($where = null,$select = null,$sort = null){//ツイートデータ取得
 	//グローバル変数宣言
 	global $mongo, $db;
 	// コレクションの選択
 	$collection = $db->selectCollection("tweetdata");
 
-	if(empty($array)){//データ取得
+	//データ取得
+	if(empty($where) and empty($select) and empty($sort)){//select,where,orderbyが指定
+		$acquisition = $collection->find($where,$select)->sort($sort);
+	}
+	elseif(empty($where) and empty($select)){//昇順・降順のみ指定
+		$acquisition = $collection->find()->sort($sort);
+	}
+	elseif(empty($where) and empty($sort)){//selectのみ指定
+		$acquisition = $collection->find(array(),$select);
+	}
+	elseif(empty($select) and empty($sort)){//whereのみ指定
+		$acquisition = $collection->find($where);
+	}
+	elseif(empty($where)){//select,orderbyが指定
+		$acquisition = $collection->find(array(),$select)->sort($sort);
+	}
+	elseif(empty($select)){//where,orderbyが指定
+		$acquisition = $collection->find($where)->sort($sort);
+	}
+	elseif(empty($sort)){//select,whereが指定
+		$acquisition = $collection->find($where,$select);
+	}
+	else{//条件なしで全件取得
 		$acquisition = $collection->find();
-	}else{
-		$acquisition = $collection->find($array);
 	}
 	//返り値
 	return $acquisition;
 }
 
-function tweets_count($array = null){
+function tweets_count($where = null){
 	//グローバル変数宣言
 	global $mongo, $db;
 	// コレクションの選択
 	$collection = $db->selectCollection("tweetdata");
 
-	if(empty($array)){//データ取得
+	if(empty($where)){//データ取得
 		$acquisition = $collection->find()->count();
 	}else{
-		$acquisition = $collection->find($array)->count();
+		$acquisition = $collection->find($where)->count();
 	}
 	//返り値
 	return $acquisition;
 }
 
-function user_search($array = null){//ユーザー情報取得
+function user_search($where = null,$select = null){//ユーザー情報取得
 	//グローバル変数宣言
 	global $mongo, $db;
 	// コレクションの選択
 	$collection = $db->selectCollection("user_data");
 
 	//データ取得
-	$acquisition = $collection->find($array);
+	if(empty($where) and empty($select)){//select,whereが指定
+		$acquisition = $collection->find($where,$select);
+	}
+	elseif(empty($where)){//selectのみ指定
+		$acquisition = $collection->find(array(),$select);
+	}
+	elseif(empty($select)){//whereのみ指定
+		$acquisition = $collection->find($where);
+	}
+	else{//条件なしで全件取得
+		$acquisition = $collection->find();
+	}
 
 	//返り値
 	return $acquisition;
 }
 
-function tweets_one_insert($array){//ツイートDBに1件挿入
+function tweets_one_insert($insert){//ツイートDBに1件挿入
 	//グローバル変数宣言
 	global $mongo, $db;
 	// コレクションの選択
 	$collection = $db->selectCollection("tweetdata");
 
 	//データ挿入
-	$collection->insert($array);
+	$collection->insert($insert);
 }
 ?>
