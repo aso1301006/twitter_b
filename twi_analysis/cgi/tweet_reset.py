f168de90cb46df926630879e78ab370ba6737672
佐藤宗大 <msyrk_n.i.f@softbank.ne.jp>
2016-11-27 17:08:50 +0900

    リセットするときの処理周りの調整

--- twi_analysis/cgi/tweet_reset.py
+++ twi_analysis/cgi/tweet_reset.py
@@ -6,10 +6,10 @@ tweetdata = mysetting.tweetdata
 
 
 if __name__ == '__main__':
-    tweetdata.update({'mecabed': True}, {'$unset': {
+    tweetdata.update({}, {'$unset': {
         'noun': 1, 'verb': 1, 'adjective': 1, 'adverb': 1, 'etc': 1,
-        'mecabed': 1, 'emotion': 1
+        'mecabed': 1, 'emotion': 1, 'emotion_point': 1
     }}, False, True)  # １つ目が upsert、２つ目が multi の指定（複数行消去するため true ）
-    print "success"
+    print mysetting.RETURN_STRING_SUCCESS
 
-print "finish"
+print mysetting.RETURN_STRING_FINISH
