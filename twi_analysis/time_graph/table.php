26ac1575be52af734ca5e886f093ab09a15db161
Owner <Owner@DESKTOP-4GNKQQD>
2016-12-01 09:21:01 +0900

    hyo

--- twi_analysis/time_graph/table.php
+++ twi_analysis/time_graph/table.php
@@ -9,6 +9,13 @@ $data = tweets_search(array("year"=>$y,"month"=>$m),array("_id"=>1,"day"=>1,"dow
 // $data = tweets_search(array("year"=>$y,"month"=>$m,"day"=>$d),null,array("hour"=>1));
 
 //----------------------------週配列--------------------------------------------------
+$first =  date('Y-m-d', strtotime('first day of ' . $y.$m.$d));//検索する月の初めを取得
+$days = get_beginning_week_date($first);//指定した日の週の日曜日の日付取得
+
+for($J=0;$J<7;$J++){//1週間
+	echo date("Y/m/d:D",strtotime("+$J day" ,strtotime($days))).'<br>';
+}
+
 //配列初期値
 $sun = array();
 $mon = array();
@@ -53,23 +60,28 @@ $count = 0;
 $w = array();
 foreach ($week as $k => $v){
 	foreach ($v as $k2 => $v2){//1週め、2週めと配列分け $w[週目][曜日] = 名詞
+		$v2['day'] = $k2;//日付追加
 		$w[$count][$k] = $v2;
 		foreach ($v2 as $k3 =>$v3){
 			if(empty($v3)){unset($w[$count][$k][$k3]);}
 		}
-		if(empty($w[$count][$k])){unset($w[$count][$k]);}
 		$count++;
 	}
 	$count = 0;
 }
+
 foreach ($w as $key => $value){
-	foreach ($value as $key2 => $value2){//1週目の曜日の最大値・最小値
-		$w[$key][$key2] = array("max_name"=>max(array_keys($value2,max($value2))),"max_value"=>max($value2),"min_name"=>min(array_keys($value2,min($value2))),"min_value"=>min($value2));
+	foreach ($value as $key2 => $value2){//1週の曜日の最大値・最小値
+		$D = $value2['day'];
+		unset($value2['day']);
+		if(!empty($value2)){
+			$w[$key][$key2] = array("day"=>$D,"max_name"=>max(array_keys($value2,max($value2))),"max_value"=>max($value2),"min_name"=>min(array_keys($value2,min($value2))),"min_value"=>min($value2));
+		}
 	}
 }
 echo '<pre>';
 print_r($w);
-print_r($week);
+// print_r($week);
 // echo max(array_keys($week['Mon'],max($week['Mon']))).':'.max($week['Mon']);
 // echo min(array_keys($week['Mon'],min($week['Mon']))).':'.min($week['Mon']);
 // print_r($sun);
@@ -112,17 +124,21 @@ echo '</pre>';
 // echo 'ネガティブ';print_r($negative);
 // echo '</pre>';
 //-------------------------------------日配列-------------------------------------
-// function page($id,$title_text){//折りたたみページを作成
-// $text = <<<EOT
-// 	<div onclick="show({$id})">
-// 		<a style="cursor:pointer;">{$title_text}</a>
-// 	</div>
-// 	<div id="{$id}" style="display:none;clear:both;">
-// 			{$id}
-// 	</div>
-// EOT;
-// 	return $text;
-// }
+function page_start($id,$title_text){//折りたたみページを作成
+$text = <<<EOT
+	<div onclick="show({$id})">
+		<a style="cursor:pointer;">{$title_text}</a>
+	</div>
+	<div id="{$id}" style="display:none;clear:both;">
+
+EOT;
+	return $text;
+}
+
+function page_fin(){
+$text = '</div>';
+return $text;
+}
 
 function cell($time,$good,$good_value,$bad,$bad_value){//折りたたみページ内のテーブル作成
 $text = <<<EOT
@@ -148,6 +164,15 @@ EOT;
 // EOT;
 // 	return $text;
 // }
+
+function get_beginning_week_date($ymd) {//指定した日の週の週初めの日付を取得
+	# もし週始まりを月曜日にするなら、$w + 1　にすればいいはずです
+	$w = date("w",strtotime($ymd));
+	$beginning_week_date =
+	date('Y-m-d', strtotime("-{$w} day", strtotime($ymd)));
+	return $beginning_week_date;
+}
+
 ?>
 
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
@@ -165,27 +190,29 @@ function show(id){
 </script>
 </head>
 <body>
-
-	<div id="point" style='border:solid 1px #AAA'>
+<?php
+//------------------------------週---------------------------------------
+$text = <<<EOT
+<div id="point" style='border:solid 1px #AAA'>
 		<div class="row">
-			<div class="time">時間</div>
+			<div class="time">曜日</div>
 			<div class="posi">ポジティブ</div>
 			<div class="posi">値</div>
 			<div class="nega">ネガティブ</div>
 			<div class="nega">値</div>
 		</div>
+EOT;
+		foreach ($w as $k => $v){
+			$title_text = ($k+1).'週目';
+			echo page_start($k, $title_text);//折り畳みページ開始
+			echo $text;
+				foreach ($v as $k2 => $v2){//表作成
+					echo cell($k2,$v2['max_name'],$v2['max_value'],$v2['min_name'],$v2['min_value']);
+				}
+				echo '</div>';
+			echo page_fin();//折り畳みページ終了
 
-		<?php
-//------------------------------週---------------------------------------
-// 		$w = array("Mon","Tue","Wed","Thu","Fri","Sat","Sun");
-// 		foreach ($w as $v){
-// 			$max_name = max(array_keys($week[$v],max($week[$v])));
-// 			$max_value = max($week[$v]);
-// 			$min_name = min(array_keys($week[$v],min($week[$v])));
-// 			$min_value = min($week[$v]);
-
-// 			echo cell($v,$max_name,$max_value,$min_name,$min_value);
-// 		}
+		}
 //------------------------------週---------------------------------------
 //---------------------------時間----------------------------------------
 		//ポジティブ名詞
@@ -204,9 +231,7 @@ function show(id){
 // 			echo cell('3:00','いいね','0.5','駄目','-0.7');
 // 		}
 //---------------------------時間-----------------------------------------
-		?>
-	</div>
-<?php
+
 // for($i=0;$i<3;$i++){
 // 	echo '<div align="center">';
 // 	echo page($i,"テスト用に作成しています_No.".$i);
