830484e30bee25aa7f43fc3cf1ed393c3bf1aa73
aso1301003 <1301003@st.asojuku.ac.jp>
2016-12-02 13:08:07 +0900

    曜日ごとのデータ取得用php追加

--- twi_analysis/week_graph/line_graph2.php
+++ twi_analysis/week_graph/line_graph2.php
@@ -2,58 +2,125 @@
 include ("/../jpgraph-4.0.1/src/jpgraph.php");
 include ("/../jpgraph-4.0.1/src/jpgraph_line.php");
 
-$ydata = array(11,3,8,12,5,1,9,13,5,7);
-$ydata2 = array(1,19,15,7,22,14,5,9,21,13);
+//サンプルデータの挿入
+//$ydata =  array(11,3,8,12,5,1,9,13,5,7,11,3,8,12,5,1,9,13,5,7,4,5,1,9);
+//$ydata2 = array(1,19,15,7,22,14,5,9,21,13,11,3,8,12,11,3,4,11,3,5,4,7,5,0);
+//$ydata3 = array(3,15,13,6,0,4,12,4,1,19,15,2,5,13,1,6,4,17,5,6,7,8,3,2);
+//$ydata4 = array(6,7,8,20,21,13,15,19,1,3,6,1,21,11,13,2,12,16,13,15,14,17,15,10);
 
-$timer = new JpgTimer();
-$timer->Push();
+//挿入可能か不明な5週目のダミーデータ
+$ydata5 = array(1,19,15,7,22,14,5,9,21,13,11,3,8,12,11,3,4,11,3,5,4,7,5,0);
 
-// Create the graph. These two calls are always required
-$graph = new Graph(700,400);
-$graph->SetScale("textlin");
+$tue_week1 =explode(",", $_GET['con_tue_week1']);
+$count=0;
+while($count<24){
+	$ydata[]=$tue_week1[$count];
+	$count++;
+}
 
-$graph->SetMargin(40,20,20,60);
+$tue_week2 =explode(",", $_GET['con_tue_week2']);
+$count=0;
+while($count<24){
+	$ydata2[]=$tue_week2[$count];
+	$count++;
+}
 
-// $graph->title->Set("Timing a graph");
-// $graph->footer->right->Set('Timer (ms): ');
-// $graph->footer->right->SetFont(FF_COURIER,FS_ITALIC);
-// $graph->footer->SetTimer($timer);
+$tue_week3 =explode(",", $_GET['con_tue_week3']);
+$count=0;
+while($count<24){
+	$ydata3[]=$tue_week3[$count];
+	$count++;
+}
 
-$title = mb_convert_encoding("火曜日", "UTF-8", "auto");
-$graph->title->Set($title);
-$graph->title->SetFont(FF_MINCHO);
+$tue_week4 =explode(",", $_GET['con_tue_week4']);
+ $count=0;
+ while($count<24){
+ 	$ydata4[]=$tue_week4[$count];
+ 	$count++;
+ }
 
-// Create the linear plot
-$lineplot=new LinePlot($ydata);
-$lineplot->SetColor("blue");
-$lineplot->SetWeight(2);
 
-$lineplot2=new LinePlot($ydata2);
-$lineplot2->SetColor("orange");
-$lineplot2->SetWeight(2);
+//$ydata5=GET['tue_week5'];
 
+
+
+ $timer = new JpgTimer();
+ $timer->Push();
+
+// // Create the graph. These two calls are always required
+ $graph = new Graph(700,400);
+ $graph->SetScale("textlin");
+
+ $graph->SetScale("textint", -1, 1);
+ $graph->yscale->ticks->Set(0.5,0.1);
+
+ $graph->SetMargin(40,20,20,60);
+
+ $graph->xaxis->SetFont(FF_GOTHIC);
+ $week=array("0","1","2","3","4","5","6","7","8","9","10","11","12","13","14","15","16","17","18","19","20","21","22","23");
+ $graph->xaxis->SetTickLabels($week);
+
+ // $graph->title->Set("Timing a graph");
+ // $graph->footer->right->Set('Timer (ms): ');
+ // $graph->footer->right->SetFont(FF_COURIER,FS_ITALIC);
+ // $graph->footer->SetTimer($timer);
+
+ //ぐらふの最大値最小値、目盛の設定
+ //$graph->SetScale("textint", -1, 1);
+ //$graph->yscale->ticks->Set(0.5,0.1);
+
+ $title = mb_convert_encoding("火曜日", "UTF-8", "auto");
+ $graph->title->Set($title);
+ $graph->title->SetFont(FF_MINCHO);
+
+ // Create the linear plot
+ $lineplot=new LinePlot($ydata);
+ $lineplot->SetColor("red");
+ $lineplot->SetWeight(2);
+
+ $lineplot2=new LinePlot($ydata2);
+ $lineplot2->SetColor("blue");
+ $lineplot2->SetWeight(2);
+
+ $lineplot3=new LinePlot($ydata3);
+ $lineplot3->SetColor("yellow");
+ $lineplot3->SetWeight(2);
+
+ $lineplot4=new LinePlot($ydata4);
+ $lineplot4->SetColor("green");
+ $lineplot4->SetWeight(2);
+
+ $lineplot5=new LinePlot($ydata5);
+ $lineplot5->SetColor("black");
+ $lineplot5->SetWeight(2);
 // Add the plot to the graph
-$graph->Add($lineplot);
-$graph->Add($lineplot2);
+ $graph->Add($lineplot);
+ $graph->Add($lineplot2);
+ $graph->Add($lineplot3);
+ $graph->Add($lineplot4);
 
-// $graph->xaxis->title->Set("X-title");
-// $graph->yaxis->title->Set("Y-title");
+ //if($mon_count==5){もし5週目が見つからない（一か月の範囲内のうちデータが4つしかない）場合、5本目をプロットしない
+ //$graph->Add($lineplot5);
+ //}
 
-// $graph->title->SetFont(FF_FONT1,FS_BOLD);
-// $graph->yaxis->title->SetFont(FF_FONT1,FS_BOLD);
-// $graph->xaxis->title->SetFont(FF_FONT1,FS_BOLD);
-$graph->xaxis->title->Set(mb_convert_encoding("時間", "UTF-8", "auto"));
-$graph->yaxis->title->Set(mb_convert_encoding("ネガポジ値", "UTF-8", "auto"));
+ // $graph->xaxis->title->Set("X-title");
+ // $graph->yaxis->title->Set("Y-title");
 
-$graph->yaxis->title->SetFont(FF_MINCHO);
-$graph->xaxis->title->SetFont(FF_MINCHO);
+ // $graph->title->SetFont(FF_FONT1,FS_BOLD);
+ // $graph->yaxis->title->SetFont(FF_FONT1,FS_BOLD);
+ // $graph->xaxis->title->SetFont(FF_FONT1,FS_BOLD);
+ $graph->xaxis->title->Set(mb_convert_encoding("時間", "UTF-8", "auto"));
+ $graph->yaxis->title->Set(mb_convert_encoding("ネガポジ値", "UTF-8", "auto"));
 
-$graph->title->SetFont(FF_MINCHO,FS_NORMAL,20);
+ $graph->yaxis->title->SetFont(FF_MINCHO);
+ $graph->xaxis->title->SetFont(FF_MINCHO);
 
-$graph->yaxis->SetColor("red");
-$graph->yaxis->SetWeight(2);
-$graph->SetShadow();
+ $graph->title->SetFont(FF_MINCHO,FS_NORMAL,20);
 
-// Display the graph
-$graph->Stroke();
+ $graph->yaxis->SetColor("red");
+ $graph->yaxis->SetWeight(2);
+ $graph->SetShadow();
+
+ // Display the graph
+ $graph->Stroke();
 ?>
---
+++ twi_analysis/week_graph/w_graph_data_mon.php
@@ -0,0 +1,200 @@
+<?php
+function num_to_str($num){
+	if(0 <= $num && $num <= 9){
+		return "0". (string)$num;
+	}
+	else{
+		return (string)$num;
+	}
+}
+ini_set("max_execution_time",180);
+
+//$n = 1; // 第n
+//$w = "月"; // w曜日
+//echo funcDesignatedDay($n, $w);先月の指定回目、指定日の日にちを取得します
+
+function funcDesignatedDay($n, $w){
+	$year_val = date("Y",strtotime("-1 month")); // 年を取得
+	$month_val =date("m",strtotime("-1 month")); // 月を取得
+
+	$arr_week = array("日", "土", "金", "木", "水", "火", "月");
+
+	for($i = 0; $i <= count($arr_week)-1; $i++){
+		if($w == $arr_week[$i]){
+			$w_no = $i;
+		}
+	}
+
+	// 曜日番号を取得(0:日曜日～6:土曜日)
+	$week_no = date('w',strtotime("$year_val/$month_val/1"));
+
+	$d = (7*$n)+1;
+	if((8 - ($week_no + $w_no)) <= 0){
+		$day = ($d - ($week_no + $w_no) + 7);
+	}else{
+		if($w_no == 0 && $week_no == 0){
+			$day = ($d - ($week_no + $w_no) - 7);
+		}else{
+			$day = ($d - ($week_no + $w_no));
+		}
+	}
+	return $day;
+}
+
+//エラー無効
+//error_reporting(0);
+
+// 24時間分のネガティブポジティブの平均値を1時間ごとに取得する
+//mongoDBから必要な値を取得
+//MongoDBクライアントの作成
+$mongo = new MongoClient("35.162.58.174:27017");
+//データベースの選択
+$db = $mongo->selectDB("twi_analysis");
+//コレクションの選択
+$collection = $db->selectCollection("tweetdata");
+//データ取得
+$acquisition = $collection->find();
+
+
+//現在日時を取得して検索条件に加える
+$year=date('Y');
+$tomonth=date('m');
+$today=date('d');
+$day_ago=date("d",strtotime("-1 day")); //１日前
+$month_ago=date("m",strtotime("-1 month")); //先月
+$year_ago=date("Y",strtotime("-1 month")); //先月の年
+$count=0;
+$sum=0;
+$negapozi=0;
+
+
+//特定月、前月、月曜,最初の週の24時間ネガポジ平均
+//day<=7,7<day<=14,14<day<=21,21<day<=28,28<day
+//day<7の0時
+//$week1_00=$collection->find(array("user_id" =>$_SESSION['id'], "year" => $year_ago, "month" => $month_ago, "dow"=>"Mon","day" => array('$lte' => "7"),"hour"=>"00"));
+//7<=day<14
+//$week2=$collection->find(array("user_id" =>$_SESSION['id'], "year" => $year_ago, "month" => $month_ago, "dow"=>"Mon","day" => array('$lte' => "14"),"day" => array('$gt' => "7")));
+//14<=day<21
+//$week3=$collection->find(array("user_id" =>$_SESSION['id'], "year" => $year_ago, "month" => $month_ago, "dow"=>"Mon","day" => array('$lte' => "21"),"day" => array('$gt' => "14")));
+//21<=day<28
+//$week4=$collection->find(array("user_id" =>$_SESSION['id'], "year" => $year_ago, "month" => $month_ago, "dow"=>"Mon","day" => array('$lte' => "28"),"day" => array('$gt' => "21")));
+//28<day
+//$week5=$collection->find(array("user_id" =>$_SESSION['id'], "year" => $year_ago, "month" => $month_ago, "dow"=>"Mon","day" => array('$gt' => "28")));
+
+
+
+
+//day<=7
+$twenty_four_count=0;
+$n = 1; // 第n
+$w = "月"; // w曜日
+$serch_day=(String)funcDesignatedDay($n, $w);
+
+while($twenty_four_count<24){
+	$hour=num_to_str($twenty_four_count);
+	$week1=$collection->find(array("user_id" =>$_SESSION['id'], "year" => $year_ago, "month" => $month_ago,"day" => $serch_day,"hour"=>$hour));
+
+	$tweet_count=$week1->count();
+	if(!$tweet_count==0){
+		foreach ($week1 as $res) {
+			$sum=$sum + ($res['emotion_point']);
+			$count=$count + 1;
+		}
+
+		$ave=$sum/$count;
+		$negapozi=round($ave,3);
+		$count=0;
+		$sum=0;
+	}
+	else{$negapozi=0;}
+	$twenty_four_count++;
+	$mon_week1[]=$negapozi;
+}
+$con_mon_week1 = implode(",", $mon_week1);
+
+
+//7<day<=14
+$twenty_four_count=0;
+$n = 2; // 第n
+$w = "月"; // w曜日
+$serch_day=(String)funcDesignatedDay($n, $w);
+while($twenty_four_count<24){
+	$hour=num_to_str($twenty_four_count);
+	$week2=$collection->find(array("user_id" =>$_SESSION['id'], "year" => $year_ago, "month" => $month_ago,"day" => $serch_day,"hour"=>$hour));
+
+	$tweet_count=$week2->count();
+	if(!$tweet_count==0){
+		foreach ($week2 as $res) {
+			$sum=$sum + ($res['emotion_point']);
+			$count=$count + 1;
+		}
+
+		$ave=$sum/$count;
+		$negapozi=round($ave,3);
+		$count=0;
+		$sum=0;
+	}
+	else{$negapozi=0;}
+	$twenty_four_count++;
+	$mon_week2[]=$negapozi;
+}
+$con_mon_week2 = implode(",", $mon_week2);
+
+
+//14<day<=21
+$twenty_four_count=0;
+$n = 3; // 第n
+$w = "月"; // w曜日
+$serch_day=(String)funcDesignatedDay($n, $w);
+while($twenty_four_count<24){
+	$hour=num_to_str($twenty_four_count);
+	$week3=$collection->find(array("user_id" =>$_SESSION['id'], "year" => $year_ago, "month" => $month_ago,"day" => $serch_day,"hour"=>$hour));
+
+	$tweet_count=$week3->count();
+	if(!$tweet_count==0){
+		foreach ($week3 as $res) {
+			$sum=$sum + ($res['emotion_point']);
+			$count=$count + 1;
+		}
+
+		$ave=$sum/$count;
+		$negapozi=round($ave,3);
+		$count=0;
+		$sum=0;
+	}
+	else{$negapozi=0;}
+	$twenty_four_count++;
+	$mon_week3[]=$negapozi;
+}
+$con_mon_week3 = implode(",", $mon_week3);
+
+
+//21<day<=28
+$twenty_four_count=0;
+$n = 4; // 第n
+$w = "月"; // w曜日
+$serch_day=(String)funcDesignatedDay($n, $w);
+while($twenty_four_count<24){
+	$hour=num_to_str($twenty_four_count);
+	$week4=$collection->find(array("user_id" =>$_SESSION['id'], "year" => $year_ago, "month" => $month_ago,"day" => $serch_day,"hour"=>$hour));
+
+$tweet_count=$week4->count();
+
+if(!$tweet_count==0){
+	foreach ($week4 as $res) {
+		$sum=$sum + ($res['emotion_point']);
+		$count=$count + 1;
+	}
+
+	$ave=$sum/$count;
+	$negapozi=round($ave,3);
+	$count=0;
+	$sum=0;
+}
+else{$negapozi=0;}
+$twenty_four_count++;
+$mon_week4[]=$negapozi;
+}
+$con_mon_week4 = implode(",", $mon_week4);
+
+ ?>
