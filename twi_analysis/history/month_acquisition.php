03acf747e78d50621356204da2c6f11e8b6f3fa8
Owner <Owner@DESKTOP-4GNKQQD>
2016-12-06 06:17:16 +0900

    自宅

--- twi_analysis/history/month_acquisition.php
+++ twi_analysis/history/month_acquisition.php
@@ -12,7 +12,22 @@
  */
 
 include '../DBManager.php';
+session_start();
 
 $y = "2018";//指定された年
 $m = "07";//指定された月
+$user_id = (string)$_SESSION['id'];
 
+//DBから値取得_月
+$data_month = tweets_search(array("year"=>$y,"month"=>$m,"user_id"=>$user_id),array("day"=>1,"emotion_point"=>1),array("month"=>1,"day"=>1));
+foreach ($data_month as $value){
+	$month[$value['day']] = $value['emotion_point'];
+}
+
+//DBから値取得_年
+$data_year = tweets_search(array("year"=>$y),array("month"=>1,"emotion_point"=>1,"user_id"=>$user_id),array("month"=>1,"day"=>1));
+
+
+echo '<pre>';
+print_r($month);
+echo '</pre>';
