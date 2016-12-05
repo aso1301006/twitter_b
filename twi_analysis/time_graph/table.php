797e89e7e1217664d73ab7bbe9058ffe56c64ab0
student <student@PC63207.asost.local>
2016-12-05 15:58:46 +0900

    時間グラフ 1週間

--- twi_analysis/time_graph/table.php
+++ twi_analysis/time_graph/table.php
@@ -1,71 +1,115 @@
 <?php
 include '../DBManager.php';
 set_time_limit(0);//処理制限時間を無期限に
-$y = '2018'; //検索する年
-$m = '07'; //検索する月
-$d = '08'; //検索する日
-//データ検索
-$data = tweets_search(array("year"=>$y,"month"=>$m,"day"=>$d),null,array("month"=>1,"day"=>1));
+$y = (string)date("Y"); //検索する年
+$m = (string)date("m"); //検索する月
+$d = (string)date("d"); //検索する日
+$D = $y.$m.$d;
+$week_ago = date("Y-m-d H:i:s",strtotime("$D -1 week"));
+$start_day = first_week_date($week_ago);//指定した日の週の日曜日の日付取得
+$end_day = fin_week_date($week_ago);//指定した日の週の土曜日の日付取得
+//----------------------------週配列--------------------------------------------------
+for($J=0;$J<7;$J++){//1週間作成
+	$date = date('Y-m-d', strtotime("$start_day +$J day"));
+	$key = date("D",strtotime($date));
+	$week[$key] = null;
+}
+$sunday = new MongoDate(strtotime(date_utc_to_jp($start_day)));
+$saturday = next_first_week_date($start_day);
+$saturday = new MongoDate(strtotime(date_utc_to_jp($saturday)));
+$data = tweets_search(array("created_at"=>array('$gt'=>$sunday, '$lte'=>$saturday)),null,array("month"=>1,"day"=>1));
 foreach ($data as $key =>$value){
-	if(isset($value['noun']) && isset($value['adjective'])){
-		$day[$value['hour']] = (array)$value['noun'];
-		$day[$value['hour']] += (array)$value['adjective'];
-	}
-	elseif(isset($value['noun'])){
-		$day[$value['hour']] = (array)$value['noun'];
-	}
-	elseif(isset($value['adjective'])){
-		$day[$value['hour']] = (array)$value['adjective'];
-	}
+	if(isset($value['noun'])){$week[$value['dow']] = $value['noun'];}
+	if(isset($value['adjective'])){$week[$value['dow']] += $value['adjective'];}
+	$week[$value['dow']] += array("date"=>$value['month'].'/'.$value['day']);
 }
 
-foreach ($day as $key =>$value){//配列weekにあるnullを取り除く
+foreach ($week as $key =>$value){//配列weekにあるnullを取り除く
 	foreach ($value as $key2 =>$value2){
-		if(empty($value2)){unset($day[$key][$key2]);}
+		if(empty($value2)){unset($week[$key][$key2]);}
 	}
 }
 
-
-foreach ($day as $key =>$value){
-	if(is_array($value)){
-		$day_time[$key] = array();//入れる箱作り
-
-		foreach ($value as $key2 =>$value2){//時ごと
-			if(!empty($day[$key][$key2])){//値が入っている場合
-				$p_name = max(array_keys($day[$key],max($day[$key])));
-				$p_value = max($day[$key]);
-				$n_name = min(array_keys($day[$key],min($day[$key])));
-				$n_value = min($day[$key]);
-
-				if($p_value <= 0){//ポジティブワードの値がマイナスの場合、削除
-					$p_name = '';
-					$p_value = '';
-					unset($day[$key][$n_name]);
-				}
-				elseif($n_value >= 0){//ネガティブワードの値がプラスの場合、削除
-					$n_name = '';
-					$n_value = '';
-					unset($day[$key][$p_name]);
-				}else{//使用済みの値を取り出す
-					unset($day[$key][$p_name]);
-					unset($day[$key][$n_name]);
-				}
-				//week_time[週][曜日][時][個数] = ポジティブワード、値、ネガティブワード、値、を挿入
-				array_push($day_time[$key], array("p_name"=>$p_name, "p_value"=>$p_value, "n_name"=>$n_name, "n_value"=>$n_value));
-			}
-			ksort($day_time);
-
-		}
+foreach ($week as $key =>$value){//配列weekの値をmax,min
+	$day = $week[$key]['date'];
+	unset($week[$key]['date']);
+	if(empty($week[$key])){
+		$max_name = '';
+		$max_value = '';
+		$min_name = '';
+		$min_value = '';
+	}else{
+		$max_name = max(array_keys($week[$key],max($week[$key])));
+		$max_value = max($week[$key]);
+		$min_name = min(array_keys($week[$key],min($week[$key])));
+		$min_value = min($week[$key]);
 	}
+	if($max_value <= 0){
+		$max_name = '';
+		$max_value = '';
+	}
+	if($min_value >= 0){
+		$min_name = '';
+		$min_value = '';
+	}
+
+	$week_day[$key] = array("max_name"=>$max_name);
+	$week_day[$key] += array("max_value"=>$max_value);
+	$week_day[$key] += array("min_name"=>$min_name);
+	$week_day[$key] += array("min_value"=>$min_value);
+	$week_day[$key] += array("date"=>$day);
 }
 
-//javascriptに配列を送るために変換
-$array = json_encode($day_time);
+function first_week_date($ymd) {//指定した日の週の週初めの日付を取得
+	$date =
+	date("Y-m-d H:i:s", strtotime("last sunday", strtotime($ymd)));
+	return $date;
+}
 
-// echo '<pre>';
-// // print_r($day);
-// print_r($day_time);
-// echo '</pre>';
+function fin_week_date($ymd) {//指定した日の週の週終わりの日付を取得
+	$date =
+	date("Y-m-d H:i:s", strtotime("next saturday", strtotime($ymd)));
+	return $date;
+}
+
+function next_first_week_date($ymd) {//指定した日の次の週初めの日付を取得
+	$date =
+	date("Y-m-d H:i:s", strtotime("next sunday", strtotime($ymd)));
+	return $date;
+}
+
+function date_utc_to_jp($utc_date){//日付を東京のタイムゾーンへ変更
+	return date("Y-m-d H:i:s", strtotime($utc_date. " +9 hour"));
+}
+
+function page_start($id,$title_text){//折りたたみページを作成
+	$text = <<<EOT
+	<div onclick="show({$id})">
+		<a style="cursor:pointer;">{$title_text}</a>
+	</div>
+	<div id="{$id}" style="display:none;clear:both;">
+
+EOT;
+	return $text;
+}
+
+function page_fin(){//折りたたみページを閉じる
+	$text = '</div>';
+	return $text;
+}
+
+function cell($time,$good,$good_value,$bad,$bad_value){//折りたたみページ内のテーブル作成
+	$text = <<<EOT
+	<div class='row'>
+		<div class="time" style="border-bottom-style: none;">{$time}</div>
+		<div>{$good}</div>
+		<div>{$good_value}</div>
+		<div>{$bad}</div>
+		<div>{$bad_value}</div>
+	</div>
+EOT;
+	return $text;
+}
 ?>
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html lang="ja" xmlns="http://www.w3.org/1999/xhtml" xml:lang="ja">
@@ -74,132 +118,36 @@ $array = json_encode($day_time);
 <link rel="stylesheet" type="text/css" href="time.css"></link>
 <link rel="stylesheet" type="text/css" href="../css/css.css"></link>
 <script type="text/javascript">
-var day = JSON.parse('<?php echo  $array; ?>');
-
-function hyo_sel(){//insert:表を挿入するdivID,
-	var Re = document.getElementById("cell");//表を挿入するdivを取得
-	Re.textContent = null;
-	folding(Re, "day_01", "今日のデータ");
-
-	var div_point = document.createElement("div");
-	div_point.id = "point";
-	div_point.style = "border:solid 1px #AAA";
-	var id = document.getElementById("day_01");
-	cell_title(id, div_point, "時間", "ポジティブワード", "数値", "ネガティブワード", "数値");
-	keys = Object.keys(day);
-	len = keys.length;
-	keys.sort();
-	for (i = 0; i < len; i++) {
-		key2 = keys[i];
-			for(key3 in day[key2]){
-			var time = key2;
-			var pName = day[key2][key3]['p_name'];
-			var pValue = day[key2][key3]['p_value'];
-			var nName = day[key2][key3]['n_name'];
-			var nValue = day[key2][key3]['n_value'];
-
-			if(key3 <= 0){
-				cell_value(true, div_point, time, pName, pValue, nName, nValue);
-			}else{
-				cell_value(false, div_point, "", pName, pValue, nName, nValue);
-			}
-		}
-	}
-
+function show(id){
+	var obj = document.getElementById(id).style;
+	var r = obj.display=(obj.display=='none')?'block':'none';
+	return r;
 }
-
-//Re:挿入先, id:何週目,
-function folding(Re, id, text){//折り畳みページを挿入
-	//折りたたみ展開ポインタ
-	var div_title = document.createElement("div");
-	div_title.onclick = function (){
-		obj=document.getElementById(id).style;
-		obj.display=(obj.display=='none')?'block':'none';
-	}
-	div_title.innerHTML = "<a style='cursor:pointer;'>"+text+"</a>";
-
-	//折りたたまれ部分
-	var div_contents = document.createElement("div");
-	div_contents.id = id;
-	div_contents.style = "display:none;clear:both;";
-
-	Re.appendChild(div_title);
-	Re.appendChild(div_contents);
-}
-
-function cell_title(Re, div_point, time, pName, pValue, nName, nValue){//Re:複製する1行の挿入先
-
-	div_row = document.createElement("div");
-	div_row.className = "row";
-
-	div_time = document.createElement("div");
-	div_time.className = "time";
-	div_time.innerHTML = time;
-
-	div_pName = document.createElement("div");
-	div_pName.className = "posi";
-	div_pName.innerHTML = pName;
-	div_pValue = document.createElement("div");
-	div_pValue.className = "posi";
-	div_pValue.innerHTML = pValue;
-
-	div_nName = document.createElement("div");
-	div_nName.className = "nega";
-	div_nName.innerHTML = nName;
-	div_nValue = document.createElement("div");
-	div_nValue.className = "nega";
-	div_nValue.innerHTML = nValue;
-
-	Re.appendChild(div_point);
-	div_point.appendChild(div_row);
-	div_row.appendChild(div_time);
-	div_row.appendChild(div_pName);
-	div_row.appendChild(div_pValue);
-	div_row.appendChild(div_nName);
-	div_row.appendChild(div_nValue);
-}
-
-function cell_value(jud, div_point, time, pName, pValue, nName, nValue){//Re:複製する1行の挿入先
-	div_row = document.createElement("div");
-	div_row.className = "row";
-
-	div_time = document.createElement("div");
-	div_time.className = "time";
-	div_time.innerHTML = time;
-	if(jud){
-		div_time.style = "border-bottom-style: none;";
-	}else{
-		div_time.style = "border-top-style: none;border-bottom-style: none;";
-	}
-
-	div_pName = document.createElement("div");
-	div_pName.className = "posi";
-	div_pName.innerHTML = pName;
-	div_pValue = document.createElement("div");
-	div_pValue.className = "posi";
-	div_pValue.innerHTML = pValue;
-
-	div_nName = document.createElement("div");
-	div_nName.className = "nega";
-	div_nName.innerHTML = nName;
-	div_nValue = document.createElement("div");
-	div_nValue.className = "nega";
-	div_nValue.innerHTML = nValue;
-
-	div_row.appendChild(div_time);
-	div_row.appendChild(div_pName);
-	div_row.appendChild(div_pValue);
-	div_row.appendChild(div_nName);
-	div_row.appendChild(div_nValue);
-	var element2 = div_row.cloneNode(true); // 要素を複製
-	div_point.appendChild(element2);
-
-}
-
-window.onload = hyo_sel;
 </script>
 </head>
 <body>
-<div id="cell"></div>
+<?php
+//------------------------------週---------------------------------------
+$text = <<<EOT
+<div id="point" style='border:solid 1px #AAA'>
+		<div class="row">
+			<div class="time">曜日</div>
+			<div class="posi">ポジティブ</div>
+			<div class="posi">値</div>
+			<div class="nega">ネガティブ</div>
+			<div class="nega">値</div>
+		</div>
+EOT;
+
+$title_text = '先週';
+echo page_start(01, $title_text);//折り畳みページ開始
+echo $text;
+foreach ($week_day as $k => $v){
+	echo cell($v['date'].' '.$k,$v['max_name'],$v['max_value'],$v['min_name'],$v['min_value']);
+}
+echo '</div>';
+echo page_fin();//折り畳みページ終了
+//------------------------------週---------------------------------------
+?>
 </body>
 </html>
--- twi_analysis/time_graph/week_max_min.php
+++ twi_analysis/time_graph/week_max_min.php
@@ -1,74 +1,63 @@
 <?php
 include '../DBManager.php';
 set_time_limit(0);//処理制限時間を無期限に
-$y = '2018'; //検索する年
-$m = '07'; //検索する月
-$d = '08'; //検索する日
-
-$start =  date("Y-m-d H:i:s", strtotime('first day of ' . $y.$m.$d));//検索する月の初めを取得
-$end = date("Y-m-d H:i:s", strtotime('last day of ' . $y.$m.$d));//検索する月の終わりを取得
-$start_day = first_week_date($start);//指定した日の週の日曜日の日付取得
-$end_day = fin_week_date($end);//指定した日の週の土曜日の日付取得
+$y = (string)date("Y"); //検索する年
+$m = (string)date("m"); //検索する月
+$d = (string)date("d"); //検索する日
+$D = $y.$m.$d;
+$week_ago = date("Y-m-d H:i:s",strtotime("$D -1 week"));
+$start_day = first_week_date($week_ago);//指定した日の週の日曜日の日付取得
+$end_day = fin_week_date($week_ago);//指定した日の週の土曜日の日付取得
 //----------------------------週配列--------------------------------------------------
-$loop = 1;//週をカウント
-while($start_day < $end_day){//week[第何週目][曜日] = 名詞+形容詞+日付
-	for($J=0;$J<7;$J++){//1週間作成
-		$date = date('Y-m-d', strtotime("$start_day +$J day"));
-		$key = date("D",strtotime($date));
-		$week[$loop][$key] = null;
-	}
-	$sunday = new MongoDate(strtotime(date_utc_to_jp($start_day)));
-	$saturday = next_first_week_date($start_day);
-	$saturday = new MongoDate(strtotime(date_utc_to_jp($saturday)));
-	$data = tweets_search(array("created_at"=>array('$gt'=>$sunday, '$lte'=>$saturday)),null,array("month"=>1,"day"=>1));
-	foreach ($data as $key =>$value){
-		if(isset($value['noun'])){$week[$loop][$value['dow']] = $value['noun'];}
-		if(isset($value['adjective'])){$week[$loop][$value['dow']] += $value['adjective'];}
-		$week[$loop][$value['dow']] += array("date"=>$value['month'].'/'.$value['day']);
-	}
-	$start_day = date('Y-m-d', strtotime('+1 week' . $start_day));
-	$key = date('D', strtotime("$key +1 week"));
-	$loop++;
+for($J=0;$J<7;$J++){//1週間作成
+	$date = date('Y-m-d', strtotime("$start_day +$J day"));
+	$key = date("D",strtotime($date));
+	$week[$key] = null;
+}
+$sunday = new MongoDate(strtotime(date_utc_to_jp($start_day)));
+$saturday = next_first_week_date($start_day);
+$saturday = new MongoDate(strtotime(date_utc_to_jp($saturday)));
+$data = tweets_search(array("created_at"=>array('$gt'=>$sunday, '$lte'=>$saturday)),null,array("month"=>1,"day"=>1));
+foreach ($data as $key =>$value){
+	if(isset($value['noun'])){$week[$value['dow']] = $value['noun'];}
+	if(isset($value['adjective'])){$week[$value['dow']] += $value['adjective'];}
+	$week[$value['dow']] += array("date"=>$value['month'].'/'.$value['day']);
 }
 
 foreach ($week as $key =>$value){//配列weekにあるnullを取り除く
 	foreach ($value as $key2 =>$value2){
-		foreach ($value2 as $key3 =>$value3){
-			if(empty($value3)){unset($week[$key][$key2][$key3]);}
-		}
+		if(empty($value2)){unset($week[$key][$key2]);}
 	}
 }
 
 foreach ($week as $key =>$value){//配列weekの値をmax,min
-	foreach ($value as $key2 =>$value2){
-		$day = $week[$key][$key2]['date'];
-		unset($week[$key][$key2]['date']);
-		if(empty($week[$key][$key2])){
-			$max_name = '～データが存在しません～';
-			$max_value = '～データが存在しません～';
-			$min_name = '～データが存在しません～';
-			$min_value = '～データが存在しません～';
-		}else{
-			$max_name = max(array_keys($week[$key][$key2],max($week[$key][$key2])));
-			$max_value = max($week[$key][$key2]);
-			$min_name = min(array_keys($week[$key][$key2],min($week[$key][$key2])));
-			$min_value = min($week[$key][$key2]);
-		}
-		if($max_value <= 0){
-			$max_name = '～データが存在しません～';
-			$max_value = '～データが存在しません～';
-		}
-		if($min_value >= 0){
-			$min_name = '～データが存在しません～';
-			$min_value = '～データが存在しません～';
-		}
-
-		$week_day[$key][$key2] = array("max_name"=>$max_name);
-		$week_day[$key][$key2] += array("max_value"=>$max_value);
-		$week_day[$key][$key2] += array("min_name"=>$min_name);
-		$week_day[$key][$key2] += array("min_value"=>$min_value);
-		$week_day[$key][$key2] += array("date"=>$day);
+	$day = $week[$key]['date'];
+	unset($week[$key]['date']);
+	if(empty($week[$key])){
+		$max_name = '';
+		$max_value = '';
+		$min_name = '';
+		$min_value = '';
+	}else{
+		$max_name = max(array_keys($week[$key],max($week[$key])));
+		$max_value = max($week[$key]);
+		$min_name = min(array_keys($week[$key],min($week[$key])));
+		$min_value = min($week[$key]);
 	}
+	if($max_value <= 0){
+		$max_name = '';
+		$max_value = '';
+	}
+	if($min_value >= 0){
+		$min_name = '';
+		$min_value = '';
+	}
+
+	$week_day[$key] = array("max_name"=>$max_name);
+	$week_day[$key] += array("max_value"=>$max_value);
+	$week_day[$key] += array("min_name"=>$min_name);
+	$week_day[$key] += array("min_value"=>$min_value);
+	$week_day[$key] += array("date"=>$day);
 }
 
 function first_week_date($ymd) {//指定した日の週の週初めの日付を取得
@@ -149,17 +138,15 @@ $text = <<<EOT
 			<div class="nega">値</div>
 		</div>
 EOT;
-foreach ($week_day as $k => $v){
-	$title_text = $k.'週目';
-	echo page_start($k, $title_text);//折り畳みページ開始
-	echo $text;
-		foreach ($v as $k2 => $v2){//表作成
-			echo cell($v2['date'].' '.$k2,$v2['max_name'],$v2['max_value'],$v2['min_name'],$v2['min_value']);
-		}
-		echo '</div>';
-	echo page_fin();//折り畳みページ終了
 
+$title_text = '先週';
+echo page_start(01, $title_text);//折り畳みページ開始
+echo $text;
+foreach ($week_day as $k => $v){
+	echo cell($v['date'].' '.$k,$v['max_name'],$v['max_value'],$v['min_name'],$v['min_value']);
 }
+echo '</div>';
+echo page_fin();//折り畳みページ終了
 //------------------------------週---------------------------------------
 ?>
 </body>
