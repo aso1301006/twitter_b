7354a2a06afbfbc91ad03cbd28589e90c874dc2e
Owner <Owner@DESKTOP-4GNKQQD>
2016-12-07 05:34:37 +0900

    サーバーデータバックアップ_12/07 05:30

--- twi_analysis/server_twi/login/common.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-//common.php
-
-define( 'CONSUMER_KEY', 'vfQ2SASQcoLdl1cqdwmMOD2yJ' );
-define( 'CONSUMER_SECRET', 'zspEuzKLR1QgraXnqaZOXxBBgTSSa0dOwyWpUYHLWnvjND7eqa' );
-define( 'OAUTH_CALLBACK', 'http://localhost/twitter_test/collback.php' );
